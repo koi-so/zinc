26a0bbff495cddc3fb2ba78511e31c6268193471
socean <lumengeared@gmail.com>
2022-10-03 23:07:28 +0800

    string templates

--- include/zinc/string.h
+++ include/zinc/string.h
@@ -10,50 +10,54 @@
 namespace zinc {
 struct string_view;
 
-template <typename TAllocator = sys_allocator<char>> struct string {
+template <typename TValue = char, typename TAllocator = sys_allocator<TValue>>
+struct basic_string {
 public:
   using size_type = usize;
 
-  inline explicit string(TAllocator const &allocator) : m_buffer(allocator) {}
-  inline string() : string(TAllocator()) {}
-  inline explicit string(size_type const count, TAllocator const &allocator)
+  inline explicit basic_string(TAllocator &allocator) : m_buffer(allocator) {}
+  inline basic_string() : basic_string(TAllocator()) {}
+  inline explicit basic_string(size_type const count, TAllocator &allocator)
       : m_buffer(count + 1, allocator) {
     m_buffer[count] = '\0';
   }
-  inline string(string_view &view, TAllocator const &allocator);
-  inline string(string_view &view) : string(view, TAllocator()) {}
+  inline basic_string(string_view &view, TAllocator &allocator);
+  inline basic_string(string_view &view) : basic_string(view, TAllocator()) {}
 
-  inline string(string const &other) : m_buffer(other.m_buffer) {
+  inline basic_string(basic_string const &other) : m_buffer(other.m_buffer) {
     m_buffer[m_buffer.size()] = '\0';
   }
-  inline string(string &&other) : m_buffer(std::move(other.m_buffer)) {}
+  inline basic_string(basic_string &&other)
+      : m_buffer(std::move(other.m_buffer)) {}
 
-  inline string(char const *str, TAllocator &allocator)
-      : string(str, strlen(str), allocator) {}
-  inline string(char const *str) : string(str, strlen(str), TAllocator()) {}
+  inline basic_string(TValue const *str, TAllocator &allocator)
+      : basic_string(str, strlen(str), allocator) {}
+  inline basic_string(TValue const *str)
+      : basic_string(str, strlen(str), TAllocator()) {}
 
-  inline string(char const *str, size_type const len, TAllocator &allocator)
+  inline basic_string(TValue const *str, size_type const len,
+                      TAllocator &allocator)
       : m_buffer(allocator) {
     m_buffer.resize(len + 1);
     memcpy(m_buffer.data(), str, len);
     m_buffer[len] = '\0';
   }
-  inline string(char const *str, size_type const len)
-      : string(str, len, TAllocator()) {}
+  inline basic_string(TValue const *str, size_type const len)
+      : basic_string(str, len, TAllocator()) {}
 
-  inline auto operator=(string const &other) -> string & {
+  inline auto operator=(basic_string const &other) -> basic_string & {
     auto len = other.length();
     m_buffer.resize(len + 1);
     memcpy(m_buffer.data(), other.data(), len);
     m_buffer[len] = '\0';
     return *this;
   }
-  inline auto operator=(string &&other) -> string & {
+  inline auto operator=(basic_string &&other) -> basic_string & {
     m_buffer = std::move(other.m_buffer);
     return *this;
   }
 
-  inline auto operator=(char const *str) -> string & {
+  inline auto operator=(TValue const *str) -> basic_string & {
     auto const len = strlen(str);
     m_buffer.resize(len + 1);
     memcpy(m_buffer.data(), str, len);
@@ -63,8 +67,8 @@ public:
 
   inline auto reserve(size_type count) -> void { m_buffer.reserve(count); }
 
-  inline auto data() -> char * { return m_buffer.data(); }
-  [[nodiscard]] inline auto data() const -> char const * {
+  inline auto data() -> TValue * { return m_buffer.data(); }
+  [[nodiscard]] inline auto data() const -> TValue const * {
     return m_buffer.data();
   }
 
@@ -76,33 +80,34 @@ public:
   // modifiers
   inline auto clear() -> void { m_buffer.clear(); }
 
-  inline auto append(char const *str) -> void {
+  inline auto append(TValue const *str) -> void {
     auto const len = strlen(str);
     append(str, len);
   }
   template <typename TOtherAllocator>
-  inline auto append(string<TOtherAllocator> const &other) -> void {
+  inline auto append(basic_string<TOtherAllocator> const &other) -> void {
     append(other.data(), other.length());
   }
-  inline auto append(char const *str, size_type const len) -> void {
+  inline auto append(TValue const *str, size_type const len) -> void {
     auto const old_size = m_buffer.size();
     m_buffer.resize(old_size + len);
     memcpy(m_buffer.data() + old_size - 1, str, len);
     m_buffer[old_size + len - 1] = '\0';
   }
 
-  inline auto operator+=(char const *str) -> string & {
+  inline auto operator+=(TValue const *str) -> basic_string & {
     append(str);
     return *this;
   }
 
   template <typename TOtherAllocator>
-  inline auto operator+=(string<TOtherAllocator> const &other) -> string & {
+  inline auto operator+=(basic_string<TOtherAllocator> const &other)
+      -> basic_string & {
     append(other);
     return *this;
   }
 
-  inline auto operator+=(char const c) -> string & {
+  inline auto operator+=(TValue const c) -> basic_string & {
     append(&c, 1);
     return *this;
   }
@@ -111,7 +116,7 @@ public:
   inline operator string_view() const;
 
 private:
-  dyn_array<char, TAllocator> m_buffer;
+  dyn_array<TValue, TAllocator> m_buffer;
 };
 
 struct string_view {
@@ -148,22 +153,28 @@ private:
   size_type m_len{0};
 };
 
-template <typename TAllocator>
-inline string<TAllocator>::string(string_view &view,
-                                  TAllocator const &allocator)
+template <typename TValue, typename TAllocator>
+inline basic_string<TValue, TAllocator>::basic_string(string_view &view,
+                                                      TAllocator &allocator)
     : m_buffer(allocator) {
   m_buffer.resize(view.length() + 1);
   memcpy(m_buffer.data(), view.data(), view.length());
   m_buffer[view.length()] = '\0';
 }
 
-template <typename TAllocator>
-inline auto string<TAllocator>::as_string_view() const -> string_view {
+template <typename TValue, typename TAllocator>
+inline auto basic_string<TValue, TAllocator>::as_string_view() const
+    -> string_view {
   return string_view{data(), length()};
 }
 
-template <typename TAllocator>
-inline string<TAllocator>::operator string_view() const {
+template <typename TValue, typename TAllocator>
+inline basic_string<TValue, TAllocator>::operator string_view() const {
   return as_string_view();
 }
+
+template <typename TAllocator = sys_allocator<char>>
+using allocator_string = basic_string<char, TAllocator>;
+
+using string = allocator_string<>;
 } // namespace zinc
--- tests/zincpp.cpp
+++ tests/zincpp.cpp
@@ -6,14 +6,19 @@ using namespace zinc;
 
 template struct zinc::option<i32>;
 
+struct m {
+  zinc::string s;
+};
+
 auto main() -> int {
   auto data_pool = zinc::pool(sizeof(char), 1000);
 
   auto pool_alloc = zinc::pool_allocator<char>{data_pool};
-  auto str = zinc::string("hi😂 ");
-  auto m = zinc::string(std::move(str));
+  auto str = zinc::string();
+  auto x = m{};
+  x.s = zinc::string("hello 😁");
   // str += zinc::string("eggs", pool_alloc);
-  std::cout << m.as_string_view().data() << std::endl;
+  std::cout << x.s.data() << std::endl;
 
   return 0;
 }
