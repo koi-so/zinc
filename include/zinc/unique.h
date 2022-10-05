accdfc2e45d5410ed8568c53199af2bd3bd351c2
socean <lumengeared@gmail.com>
2022-10-05 17:31:05 +0800

    better unique

--- include/zinc/unique.h
+++ include/zinc/unique.h
@@ -2,92 +2,72 @@
 
 #include "base.h"
 
-namespace zinc
-{
-  template <typename TValue>
-  struct unique
-  {
-  public:
-    template <typename OtherDataType>
-    friend struct unique;
+namespace zinc {
+template <typename T> struct unique {
+public:
+  template <typename OtherDataType> friend struct unique;
 
-    explicit unique(TValue *raw_ptr) noexcept : m_raw_ptr(std::move(raw_ptr)) {}
+  explicit unique(T *raw_ptr) noexcept : m_raw_ptr(std::move(raw_ptr)) {}
 
-    unique(std::nullptr_t) noexcept : m_raw_ptr(nullptr) {}
-    unique() noexcept : m_raw_ptr(nullptr) {}
+  unique(std::nullptr_t) noexcept : m_raw_ptr(nullptr) {}
+  unique() noexcept : m_raw_ptr(nullptr) {}
 
-    ~unique()
-    {
-      if (m_raw_ptr)
-        delete (m_raw_ptr);
-    }
+  ~unique() { cleanup(); }
 
-    unique(const unique &other) = delete;
-    auto operator=(const unique &other) -> unique & = delete;
+  unique(const unique &other) = delete;
+  auto operator=(const unique &other) -> unique & = delete;
 
-    void swap(unique &other_owned) noexcept
-    {
-      std::swap(m_raw_ptr, other_owned.m_raw_ptr);
-    }
-
-    unique(unique &&other) noexcept : m_raw_ptr(other.m_raw_ptr)
-    {
-      other.m_raw_ptr = nullptr;
-    }
-
-    template <typename OtherDataType,
-              typename = safe_upcast<OtherDataType, TValue>>
-    unique(unique<OtherDataType> &&other) noexcept : m_raw_ptr(other.release()) {}
-
-    auto operator=(unique &&other) noexcept -> unique &
-    {
-      other.swap(*this);
-      return *this;
-    }
-
-    template <typename OtherDataType>
-    auto operator=(unique<OtherDataType> &&other) noexcept -> unique &
-    {
-      other.swap(*this);
-      return *this;
-    }
-
-    [[nodiscard]] explicit operator bool() const noexcept
-    {
-      return m_raw_ptr != nullptr;
-    }
-
-    [[nodiscard]] auto release() noexcept -> TValue *
-    {
-      return std::exchange(m_raw_ptr, nullptr);
-    }
-
-    [[nodiscard]] auto get() const noexcept -> TValue * { return m_raw_ptr; }
-
-    void reset(TValue *other_owned)
-    {
-      m_deleter(m_raw_ptr);
-      m_raw_ptr = nullptr;
-      std::swap(m_raw_ptr, other_owned);
-    }
-
-    // operators
-    [[nodiscard]] auto operator->() const noexcept -> TValue *
-    {
-      return m_raw_ptr;
-    }
-    [[nodiscard]] auto operator*() const noexcept -> TValue &
-    {
-      return *m_raw_ptr;
-    }
-
-  private:
-    TValue *m_raw_ptr{nullptr};
-  };
-
-  template <typename TValue, typename... Args>
-  auto make_unique(Args &&...args) -> unique<TValue>
-  {
-    return unique<TValue>(new TValue(std::forward<Args>(args)...));
+  void swap(unique &other_owned) noexcept {
+    std::swap(m_raw_ptr, other_owned.m_raw_ptr);
   }
+
+  unique(unique &&other) noexcept { std::swap(m_raw_ptr, other.m_raw_ptr); }
+
+  template <typename OtherDataType, typename = safe_upcast<OtherDataType, T>>
+  unique(unique<OtherDataType> &&other) noexcept : m_raw_ptr(other.release()) {}
+
+  auto operator=(unique &&other) noexcept -> unique & {
+    cleanup();
+    other.swap(*this);
+    return *this;
+  }
+
+  template <typename OtherDataType>
+  auto operator=(unique<OtherDataType> &&other) noexcept -> unique & {
+    cleanup();
+    other.swap(*this);
+    return *this;
+  }
+
+  [[nodiscard]] explicit operator bool() const noexcept {
+    return m_raw_ptr != nullptr;
+  }
+
+  [[nodiscard]] auto release() noexcept -> T * {
+    return std::exchange(m_raw_ptr, nullptr);
+  }
+
+  [[nodiscard]] auto get() const noexcept -> T * { return m_raw_ptr; }
+
+  void reset(T *other_owned) {
+    m_raw_ptr = nullptr;
+    std::swap(m_raw_ptr, other_owned);
+  }
+
+  // operators
+  [[nodiscard]] auto operator->() const noexcept -> T * { return m_raw_ptr; }
+  [[nodiscard]] auto operator*() const noexcept -> T & { return *m_raw_ptr; }
+
+private:
+  void cleanup() {
+    if (m_raw_ptr != nullptr)
+      delete (m_raw_ptr);
+  }
+  T *m_raw_ptr{nullptr};
+};
+
+template <typename T, typename... Args>
+auto make_unique(Args &&...args) -> unique<T> {
+  return std::move(unique<T>(new T(std::forward<Args>(args)...)));
+}
 } // namespace zinc
