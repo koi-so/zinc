9a07c361e21aace86e3a020bc9489f4937d321c8
socean <lumengeared@gmail.com>
2022-10-23 23:01:47 +0800

    interface

---
+++ include/zinc/interface.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace zinc {
+class interface {
+public:
+  virtual ~interface() = default;
+
+  template <typename T> auto as() -> T & { return static_cast<T &>(*this); }
+
+  template <typename T> auto as() const & -> T const & {
+    return static_cast<T &>(*this);
+  }
+};
+} // namespace zinc
