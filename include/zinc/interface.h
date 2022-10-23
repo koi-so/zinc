9a7300283d412f93522fbf39c3ee55bb4a724c44
socean <lumengeared@gmail.com>
2022-10-23 23:03:17 +0800

    ref qualifier

--- include/zinc/interface.h
+++ include/zinc/interface.h
@@ -5,7 +5,7 @@ class interface {
 public:
   virtual ~interface() = default;
 
-  template <typename T> auto as() -> T & { return static_cast<T &>(*this); }
+  template <typename T> auto as() & -> T & { return static_cast<T &>(*this); }
 
   template <typename T> auto as() const & -> T const & {
     return static_cast<T &>(*this);
